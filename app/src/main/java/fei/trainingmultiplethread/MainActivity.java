2d370bf1e17f57677c85f5d19f26ebb408670df7
lsx <lsx.office.ubuntu@163.com>
2016-07-29 17:47:45 +0800

    layout实现

--- app/src/main/java/fei/trainingmultiplethread/MainActivity.java
+++ app/src/main/java/fei/trainingmultiplethread/MainActivity.java
@@ -2,12 +2,20 @@ package fei.trainingmultiplethread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView mImageView;
+    private Button mLoadImageButton;
+    private Button mShowToastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.activity_main_image_view);
+        mLoadImageButton = (Button) findViewById(R.id.activity_main_load_image_button);
+        mShowToastButton = (Button) findViewById(R.id.activity_main_show_toast_button);
     }
 }
