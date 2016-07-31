3b2498f2decd990290f2ba34a1d4c5dfe0ac1420
lsx <lsx.office.ubuntu@163.com>
2016-07-31 14:26:25 +0800

    user handler

--- app/src/main/java/fei/trainingmultiplethread/MainActivity.java
+++ app/src/main/java/fei/trainingmultiplethread/MainActivity.java
@@ -2,8 +2,9 @@ package fei.trainingmultiplethread;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private Button mShowToastButton;
     private ProgressBar mProgressBar;
     private static final int mSleepTime = 500;
+    private static final int SET_PROGRESS_BAR_VISIBLE = 0;
+    private static final int UPDATE_PROGRESS_BAR = 1;
+    private static final int UPDATE_IMAGE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +30,25 @@ public class MainActivity extends AppCompatActivity {
         mLoadImageButton = (Button) findViewById(R.id.activity_main_load_image_button);
         mShowToastButton = (Button) findViewById(R.id.activity_main_show_toast_button);
         mProgressBar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
-
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case SET_PROGRESS_BAR_VISIBLE:
+                        mProgressBar.setVisibility((Integer)msg.obj);
+                        break;
+                    case UPDATE_PROGRESS_BAR:
+                        mProgressBar.setProgress((Integer) msg.obj);
+                        break;
+                    case UPDATE_IMAGE:
+                        mImageView.setImageBitmap((Bitmap) msg.obj);
+                }
+            }
+        };
         mLoadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new LoadImageTask().execute();
+               new Thread(new LoadImageTask(handler)).start();
             }
         });
 
@@ -51,32 +69,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    class LoadImageTask extends AsyncTask<Void, Integer, Bitmap> {
-        @Override
-        protected void onPreExecute() {
-            mProgressBar.setVisibility(View.VISIBLE);
+    class LoadImageTask implements Runnable {
+        private Handler mHandler;
+
+        public LoadImageTask(Handler handler) {
+            mHandler = handler;
         }
 
         @Override
-        protected Bitmap doInBackground(Void... params) {
+        public void run() {
+            Message msg = null;
+            msg = mHandler.obtainMessage(SET_PROGRESS_BAR_VISIBLE, ProgressBar.VISIBLE);
+            mHandler.sendMessage(msg);
             for (int i = 1; i < 11; i++) {
                 sleep(500);
-                publishProgress(i * 10);
+                msg = mHandler.obtainMessage(UPDATE_PROGRESS_BAR, i * 10);
+                mHandler.sendMessage(msg);
             }
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
-            return bitmap;
-        }
+            msg = mHandler.obtainMessage(UPDATE_IMAGE, bitmap);
+            mHandler.sendMessage(msg);
+            msg = mHandler.obtainMessage(SET_PROGRESS_BAR_VISIBLE, ProgressBar.INVISIBLE);
+            mHandler.sendMessage(msg);
 
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            mProgressBar.setVisibility(View.INVISIBLE);
-            mImageView.setImageBitmap(bitmap);
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            mProgressBar.setProgress(values[0]);
         }
     }
+
 
 }
