8041a40585459a1b07ed8b12f4d3769b2a8fbd9a
lsx <lsx.office.ubuntu@163.com>
2016-07-31 08:13:55 +0800

    horizontal progress bar

--- app/src/main/java/fei/trainingmultiplethread/MainActivity.java
+++ app/src/main/java/fei/trainingmultiplethread/MainActivity.java
@@ -8,12 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView mImageView;
     private Button mLoadImageButton;
     private Button mShowToastButton;
+    private ProgressBar mProgressBar;
     private static final int mSleepTime = 500;
 
     @Override
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         mImageView = (ImageView) findViewById(R.id.activity_main_image_view);
         mLoadImageButton = (Button) findViewById(R.id.activity_main_load_image_button);
         mShowToastButton = (Button) findViewById(R.id.activity_main_show_toast_button);
+        mProgressBar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
 
         mLoadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,15 +53,29 @@ public class MainActivity extends AppCompatActivity {
 
     class LoadImageTask extends AsyncTask<Void, Integer, Bitmap> {
         @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected Bitmap doInBackground(Void... params) {
-            sleep(4000);
+            for (int i = 1; i < 11; i++) {
+                sleep(500);
+                publishProgress(i * 10);
+            }
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
             return bitmap;
         }
-    
+
         @Override
         protected void onPostExecute(Bitmap bitmap) {
+            mProgressBar.setVisibility(View.INVISIBLE);
             mImageView.setImageBitmap(bitmap);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressBar.setProgress(values[0]);
         }
     }
 
